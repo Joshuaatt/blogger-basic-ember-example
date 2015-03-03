8a69421ea0851f8fc2130688fa9f7983565b7902
Joshua Atteberry <joshuaatteberry@gmail.com>
2015-03-03 11:50:29 -0800

    add sample data

---
+++ store.js
@@ -0,0 +1,59 @@
+var posts = [
+  {
+    id: '1',
+    title: "I love blood",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+    Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit,
+    sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. N"
+  },
+  {
+    id: '2',
+    title: "The count",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+    ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+    odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+    voluptatem sequi nesciunt. N"
+  },
+  {
+    id: '3',
+    title: "Something else",
+    body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+    ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+    explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+    odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+    voluptatem sequi nesciunt. N"
+  }
+];
+
+var comments = [
+  {
+    id: '1',
+    postId: '3',
+    text: 'What a shame'
+  },
+  {
+    id: '2',
+    postId: '3',
+    text: 'Blah blah blah'
+  },
+  {
+    id: '3',
+    postId: '1',
+    text: 'SOmething something something'
+  },
+  {
+    id: '4',
+    postId: '2',
+    text: 'tree tree tree'
+  },
+  {
+    id: '5',
+    postId: '2',
+    text: 'flower flower flower'
+  }
+]
